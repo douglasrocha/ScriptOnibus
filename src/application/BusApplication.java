74b25a918eee2465727a21d6339e4772f6865b33
Douglas Rocha <douglasbellonrocha@gmail.com>
2015-07-31 10:20:32 -0300

    Tratamento de exception de obtenção de JSON

--- src/application/BusApplication.java
+++ src/application/BusApplication.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import domain.entities.Bus;
@@ -8,13 +9,21 @@ import domain.services.BusService;
 
 public class BusApplication
 {
-    public static List<Bus> get(int linha) throws IOException
+    public static List<Bus> get(int linha)
     {       
         // Jackson does not accept uppercase properties
-        String content = BusService.getLineJson(linha).replace("COLUMNS", "columns")
-                                                      .replace("DATA", "data");
-        
-        return BusService.getListOfBusFromJson(content);
+        String content;
+        try
+        {
+            content = BusService.getLineJson(linha).replace("COLUMNS", "columns")
+                                                          .replace("DATA", "data");
+            
+            return BusService.getListOfBusFromJson(content);
+        }
+        catch (IOException e)
+        {
+            return new ArrayList<Bus>();
+        }
     }
     
     public static boolean insert(Bus bus)
--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -1,6 +1,5 @@
 package main;
 
-import java.io.IOException;
 import java.util.List;
 
 import application.BusApplication;
@@ -10,63 +9,32 @@ public class MainClass
 {
 
     public static void main(String[] args)
-    {
-        try
+    {        
+        while (true)
         {
-            while (true)
-            {
+        
+            List<Bus> list570 = BusApplication.get(570);
+            List<Bus> list583 = BusApplication.get(583);
             
-                List<Bus> list570 = BusApplication.get(570);
-                List<Bus> list583 = BusApplication.get(583);
-                
-                /*
-                System.out.println("Linha 570");
-                
-                for (Bus bus : list570)
-                {
-                    System.out.println("Ordem -> " + bus.getOrdem() + 
-                                       "; Latitude -> " + bus.getLatidude() + 
-                                       "; Longitude -> " + bus.getLongitude());
-                }
-                
-                System.out.println("--------------");
-                
-                System.out.println("Linha 583");
-                
-                for (Bus bus : list583)
-                {
-                    System.out.println("Ordem -> " + bus.getOrdem() + 
-                                       "; Latitude -> " + bus.getLatidude() + 
-                                       "; Longitude -> " + bus.getLongitude());
-                }
-                
-                System.out.println();
-                */
-                
-                for (Bus bus : list570)
-                {
-                    BusApplication.insert(bus);
-                }
-                
-                for (Bus bus : list583)
-                {
-                    BusApplication.insert(bus);
-                }
-                
-                try 
-                {
-                    Thread.sleep(300000);
-                    //Thread.sleep(5000);
-                } 
-                catch(InterruptedException ex) 
-                {
-                    Thread.currentThread().interrupt();
-                }
+            for (Bus bus : list570)
+            {
+                BusApplication.insert(bus);
             }
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
+            
+            for (Bus bus : list583)
+            {
+                BusApplication.insert(bus);
+            }
+            
+            try 
+            {
+                Thread.sleep(300000);
+                //Thread.sleep(5000);
+            } 
+            catch(InterruptedException ex) 
+            {
+                Thread.currentThread().interrupt();
+            }
         }
     }
 
